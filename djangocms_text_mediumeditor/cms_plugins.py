e5338e186a5da1ef4390a60810f22a798a5593a6
Mikołaj Giermek <mikolaj.giermek@gmail.com>
2021-05-04 12:32:17 +0200

    test: Popup is set to False in plugin declaration

--- djangocms_text_mediumeditor/cms_plugins.py
+++ djangocms_text_mediumeditor/cms_plugins.py
@@ -6,6 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
+from cms.utils.conf import get_cms_setting
 
 from .forms import MediumEditorTextForm
 from .models import MediumEditorText
@@ -25,6 +26,19 @@ class MediumEditorTextPlugin(CMSPluginBase):
             "object": instance,
         })
         return context
+
+    def render_change_form(self, request, context, add=False, change=False, form_url='', obj=None):
+        """
+        We just need the popup interface here
+        """
+        context.update({
+            'preview': "no_preview" not in request.GET,
+            'is_popup': False,
+            'plugin': obj,
+            'CMS_MEDIA_URL': get_cms_setting('MEDIA_URL'),
+        })
+
+        return super().render_change_form(request, context, add, change, form_url, obj)
 
 
 plugin_pool.register_plugin(MediumEditorTextPlugin)
